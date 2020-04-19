55be3ffbcb454eb9368208b76f6f171703ad202f
alexanderr <alexanderr@users.noreply.github.com>
2020-04-19 02:56:57 -0500

    otp: you can now run the cluster as a whole

--- otp/otp.py
+++ otp/otp.py
@@ -1,14 +1,16 @@
 import os
 from multiprocessing import Pool
 
+import sys
+
 
 def run_process(process):
-    os.system('python3.7 {}'.format(process))
+    os.system(f'{sys.executable} {process}')
 
 
 def main():
-    pool = Pool(processes=3)
-    pool.map(run_process, ('messagedirector.py',))
+    pool = Pool(processes=4)
+    pool.map(run_process, ('-m otp.messagedirector', '-m otp.dbserver', '-m otp.stateserver', '-m otp.clientagent'))
 
 
 if __name__ == "__main__":
