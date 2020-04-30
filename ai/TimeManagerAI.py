941e8ea8d3e0b09bfb4207a3c8ba5390ecbd112d
John Cote <jwcotejr@users.noreply.github.com>
2020-04-30 14:39:57 -0400

    ai: more for TimeManagerAI

--- ai/TimeManagerAI.py
+++ ai/TimeManagerAI.py
@@ -13,6 +13,7 @@ class TimeManagerAI(DistributedObjectAI):
     def __init__(self, air):
         DistributedObjectAI.__init__(self, air)
         self.avId2disconnectcode = {}
+        self.avId2exceptioninfo = {}
 
     def requestServerTime(self, context):
         avId = self.air.getAvatarIdFromSender()
@@ -30,3 +31,31 @@ class TimeManagerAI(DistributedObjectAI):
         self.avId2disconnectcode[avId] = disconnectCode
         self.air.writeServerEvent('disconnect-reason', avId=avId,
                                   reason=OTPGlobals.DisconnectReasons.get(disconnectCode, 'unknown'))
+
+    def setExceptionInfo(self, info):
+        avId = self.air.getAvatarIdFromSender()
+        if not avId:
+            return
+
+        self.avId2exceptioninfo[avId] = info
+        self.air.writeServerEvent('client-exception', avId=avId, info=info)
+
+    def setSignature(self, signature, fileHash, pyc):
+        pass  # TODO
+
+    def setFrameRate(self, fps, deviation, numAvs, locationCode, timeInLocation, timeInGame, gameOptionsCode, vendorId,
+                     deviceId, processMemory, pageFileUsage, physicalMemory, pageFaultCount, osInfo, cpuSpeed,
+                     numCpuCores, numLogicalCpus, apiName):
+        pass  # TODO
+
+    def setCpuInfo(self, info, cacheStatus):
+        pass  # TODO
+
+    def checkForGarbageLeaks(self, wantReply):
+        pass  # TODO
+
+    def setClientGarbageLeak(self, num, description):
+        pass  # TODO
+
+    def checkAvOnDistrict(self, context, avId):
+        pass  # TODO
