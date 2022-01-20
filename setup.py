bfe33919ddfd8b62f9dae8058ebfa5e68730eb87
alexanderr <localtoasted@pm.me>
2022-01-19 22:47:46 -0600

    add setup.py for package installation

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='OpenOTP',
+      version='0.1',
+      description='Python module for emulation of Disney\'s Distributed network system',
+      author='alexanderr',
+      author_email='localtoasted@pm.me',
+      url='https://www.github.com/Open-OTP/OpenOTP',
+      packages=['otp', 'par']
+      )
